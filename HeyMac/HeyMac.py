9e54df8053ba76b0e6403bde3c853119d3de3d7d
Dean Hall <dwhall256@gmail.com>
2017-08-05 21:59:08 -0500

    Eschew import *, protect the namespace

--- HeyMac/HeyMac.py
+++ HeyMac/HeyMac.py
@@ -5,32 +5,31 @@ Copyright 2017 Dean Hall.  See LICENSE file for details.
 
 import asyncio
 
-from lora_driver import *
-from pq import *
+import lora_driver, pq
 
 
-class HeyMac(Ahsm):
+class HeyMac(pq.Ahsm):
     """Highly Engineered Yodeling, Medium Access Control
     A class that offers high-level control of the SX127x device
     by using instances of SX127xSpi and SX127xGpio from lora_driver.
     """
-    def __init__(self, GpioClass=DraginoLoraGpsHat):
+    def __init__(self, GpioClass=lora_driver.DraginoLoraGpsHat):
 
         # Init this Ahsm with the initial pseudo-state
         super().__init__(HeyMac.initial)
 
         # Instances to control GPIO and SPI peripherals
         self.gpio = GpioClass()
-        self.spi = SX127xSpi()
+        self.spi = lora_driver.SX127xSpi()
 
         # Packet receive and transmit queues
-        self.rx_queue = Queue()
-        self.tx_queue = Queue()
+        self.rx_queue = lora_driver.Queue()
+        self.tx_queue = lora_driver.Queue()
 
         # Declare DIO events
-        self.evt_dio0 = Event(Signal.register("MAC_DIO0"), None)
-        self.evt_dio1 = Event(Signal.register("MAC_DIO1"), None)
-        self.evt_dio2 = Event(Signal.register("MAC_DIO2"), None)
+        self.evt_dio0 = pq.Event(pq.Signal.register("MAC_DIO0"), None)
+        self.evt_dio1 = pq.Event(pq.Signal.register("MAC_DIO1"), None)
+        self.evt_dio2 = pq.Event(pq.Signal.register("MAC_DIO2"), None)
 
         # Set GPIO handlers that emit DIO events
         self.gpio.set_dio0_handler(self.dio0_handler)
@@ -43,9 +42,9 @@ class HeyMac(Ahsm):
     # So in the following GPIO handlers, we enqueue a unique event for each GPIO.
     # The separate thread will exit quickly back to the main thread
     # and the event will be processed there.
-    def dio0_handler(self,): Framework.publish(self.evt_dio0)
-    def dio1_handler(self,): Framework.publish(self.evt_dio1)
-    def dio2_handler(self,): Framework.publish(self.evt_dio2)
+    def dio0_handler(self,): pq.Framework.publish(self.evt_dio0)
+    def dio1_handler(self,): pq.Framework.publish(self.evt_dio1)
+    def dio2_handler(self,): pq.Framework.publish(self.evt_dio2)
 
 
     @staticmethod
@@ -60,12 +59,12 @@ class HeyMac(Ahsm):
         """State: Initialiaing
         """
         sig = event.signal
-        if sig == Signal.ENTRY:
-            print("Initializing...")
+        if sig == pq.Signal.ENTRY:
+            print("HeyMac Initializing...") # TODO: logging
             if me.spi.check_version():
-                print("SPI to LoRa: PASS")
+                print("SPI to LoRa: PASS") # TODO: state transition
             else:
-                pass
+                print("SPI to LoRa: FAIL")
             return me.handled(me, event)
         return me.super(me, me.top)
 
