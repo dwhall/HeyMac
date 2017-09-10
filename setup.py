5e405db87cb744d0da3c6303b6af30e2747b65fc
Dean Hall <dwhall256@gmail.com>
2017-09-10 10:58:43 -0500

    Added distutils setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="HeyMac",
+      version="0.1",
+      description="Data Link Layer (2) protocol for low-power, lossy RF data"
+      author="Dean Hall"
+      author_email="dwhall256@gmail.com"
+      url="https://github.com/dwhall/HeyMac"
+      packages=['HeyMac',],
+     )
