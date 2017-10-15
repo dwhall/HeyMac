17391f1964ae7083f8159025a8287d19ef1ab4f3
Dean Hall <dwhall256@gmail.com>
2017-10-15 11:20:04 -0500

    Adding file details

--- HeyMac/gps_cfg.py
+++ HeyMac/gps_cfg.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
+"""
+Copyright 2017 Dean Hall.  See LICENSE file for details.
+"""
+
+
 class GpsCfg(object):
     pass
 
 
 class DraginoLoraGpsHat(GpsCfg):
+    # PCB version 1.3
     # WIRING REQUIREMENT: connect GPS module's PPS pin 6 to RPi's GPIO26 pin 37
     serial_port = "/dev/serial0"
     serial_baud = 9600
