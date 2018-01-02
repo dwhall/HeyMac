e0231397eca647a1e2ac296d5fe31c2fe37589b6
Dean Hall <dwhall256@gmail.com>
2018-01-01 23:03:05 -0600

    Start filling out the beacon structure

--- heymac/mac_cmds.py
+++ heymac/mac_cmds.py
@@ -17,9 +17,19 @@ class HeyMacCmdBeacon(dpkt.Packet):
     __hdr__ = (
         ('cmd', 'B', HEYMAC_CMD_BEACON),
         ('asn', 'I', 0),
-        ('slotmap', 'I', 0),
+        ('slotmap', 'I', 0), # REMOVE after old beacon is deprecated
+#        ('disc_src', 'B', b"\x00"),     # 0:None, 1:RF, 2:GPS
+#        ('disc_hops', 'B', b"\x00"),
+#        ('caps', 'I', 0),   # capabilities
+#        ('flags', 'I', 0),  # status flags
+#        ('sframe_nchnls', 'B', b"\x01"),
+#        ('sframe_hopLutId', 'B', b"\x00"),
+#        ('sframe_nSlots', 'H', 0),
+#        ('tx_slotmap', '4I', 0),    # 128 Tslots
+#        ('ngbr_slotmap', '4I', 0),  # 128 Tslots
         # variable-length fields
 # TODO:
 #        ('_nghbrs', '0s', b''),
 #        ('_ntwks', '0s', b''),
+#        ('_geoloc', '0s', b'')
     )
