b6159c06c16b626ee3002b95f70298585a69ae31
Dean Hall <dwhall256@gmail.com>
2018-05-18 17:11:01 -0500

    Update the setup script: fixed sytax, added requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from distutils.core import setup
 
 setup(name="HeyMac",
       version="0.1",
-      description="Data Link Layer (2) protocol for low-power, lossy RF data"
-      author="Dean Hall"
-      author_email="dwhall256@gmail.com"
-      url="https://github.com/dwhall/HeyMac"
+      description="Data Link Layer (2) protocol for low-power, lossy wireless data transfer",
+      author="Dean Hall",
+      author_email="dwhall256@gmail.com",
+      url="https://github.com/dwhall/HeyMac",
       packages=['HeyMac',],
+      requires=[
+# These aren't in PyPI yet
+#            "lora_driver",
+#            "pq",
+            "dpkt",
+            "pyserial",
+            "RPi",
+            ],
      )
