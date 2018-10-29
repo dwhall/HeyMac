c52f214bedc4e076724156a2b80a96e5b0cd13e1
Dean Hall <dwhall256@gmail.com>
2018-10-29 16:44:25 -0500

    Rewrote line in Py3.4 syntax, added feb/12 address generation

--- heymac/gen_identity.py
+++ heymac/gen_identity.py
@@ -8,6 +8,7 @@ An asymmetric cryptographic keypair is generated
 such that the SHA512(SHA512(pub_key)) starts with 0xFC or 0xFD
 """
 
+import codecs
 import hashlib
 import json
 import os.path
@@ -31,6 +32,13 @@ def gen_personal_keypair():
 def gen_device_keypair():
     return _gen_specific_keypair("fd")
 
+def gen_feb12b_keypair():
+    """Generates a feb/12 keypair.
+    Note that feb/12 is a subset of fe8/10
+    the well-known link-local prefix.
+    """
+    return _gen_specific_keypair("feb")
+
 def _gen_specific_keypair(prefix):
     """Generates keypairs and forgets them
     until one is made with a specific prefix (nibble, byte or bytes).
@@ -48,13 +56,15 @@ def _gen_specific_keypair(prefix):
 if __name__ == "__main__":
     name = input("Full name: ")
     callsign = input("Callsign: ")
-    _, pub_key = gen_device_keypair()
+    _, pub_key = gen_feb12b_keypair()
 
     fn = os.path.join(utl.get_app_data_path("HeyMac"), "mac_identity.json")
     with open(fn, 'w') as f:
         json_str = json.dumps({
             "name": name,
             "callsign": callsign,
-            "pub_key": pub_key.hex()})
+            # "pub_key": pub_key.hex()}) # Python 3.5 and later
+            "pub_key": codecs.encode(pub_key, 'hex').decode() # Python 3.4
+            })
         f.write(json_str)
     print("Wrote: %s" % fn)
