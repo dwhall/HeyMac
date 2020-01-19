1a9cce1725f55abe3ebd3e899783f96ff06416b4
Dean Hall <dwhall256@gmail.com>
2020-01-18 23:45:17 -0600

    Implemented elliptic curve algo for identity credential
    Note that an asymmetric key pair is generated and the private key is saved in an insecure fashion.  This is acceptable because the goal isn't encryption, it's authentication.

--- scripts/heymac_gen_identity.py
+++ scripts/heymac_gen_identity.py
@@ -6,6 +6,15 @@ Copyright 2017 Dean Hall.  See LICENSE for details.
 This is a tool to generate a HeyMac identity configuration file.
 An asymmetric cryptographic keypair is generated
 such that the SHA512(SHA512(pub_key)) starts with 0xFC or 0xFD
+
+Dependencies::
+
+    pip install asn1
+    pip install crytography
+
+References:
+    https://cryptography.io/en/latest/
+    https://github.com/andrivet/python-asn1/blob/master/examples/dump.py
 """
 
 import codecs
@@ -14,16 +23,19 @@ import json
 import os.path
 import random
 
+import asn1 # pip install asn1
+from cryptography.hazmat.backends import default_backend # pip install cryptography
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+
 from heymac import utl
 
 
 def gen_keypair():
-    """Generates 256 bit keypair
+    """Generates asymmetric keypair using elliptic curve SECP384R1
     """
-    # TODO: run system() to call real crypto tool to generate a key pair
-    prv_key = bytes(bytearray(32))
-    pub_key = random.getrandbits(256).to_bytes(32, 'big')
-    return (prv_key, pub_key)
+    prv_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
+    return (prv_key, prv_key.public_key())
 
 
 def gen_personal_keypair():
@@ -46,25 +58,79 @@ def _gen_specific_keypair(prefix):
     done = False
     while not done:
         prv_key, pub_key = gen_keypair()
+        der_bytes = pub_key.public_bytes(
+            serialization.Encoding.DER,
+            serialization.PublicFormat.SubjectPublicKeyInfo)
+        pub_key_bytes = _get_key_from_asn1(der_bytes)
         h = hashlib.sha512()
-        h.update(pub_key)
+        h.update(pub_key_bytes)
         h.update(h.digest())
         done = h.hexdigest().startswith(prefix)
     return (prv_key, pub_key)
 
 
+def _get_key_from_asn1(der_bytes):
+    """Returns the key bytes from a PublicKey instance
+    whose DER encoding resembles:
+    [U] SEQUENCE
+        [U] SEQUENCE
+            [U] OBJECT: 1.2.840.10045.2.1
+            [U] OBJECT: 1.3.132.0.34
+        [U] BIT STRING:<key bytes>
+    """
+    decoder = asn1.Decoder()
+    decoder.start(der_bytes)
+    def rdparse_asn1(decoder):
+        retval = None
+        while not decoder.eof():
+            tag = decoder.peek()
+            if tag.typ == asn1.Types.Primitive:
+                tag, retval = decoder.read()
+                if tag.cls == asn1.Numbers.BitString:
+                    break
+            elif tag.typ == asn1.Types.Constructed:
+                decoder.enter()
+                retval = rdparse_asn1(decoder)
+                decoder.leave()
+        return retval
+    return rdparse_asn1(decoder)
+
+
 if __name__ == "__main__":
     name = input("Full name: ")
     callsign = input("Callsign: ")
-    _, pub_key = gen_personal_keypair()
+    passphrase = input("Private key encryption passphrase: ")  # sketchy
+    passphrase = passphrase.encode()
 
+    prv_key, pub_key = gen_personal_keypair()
+    der_bytes = pub_key.public_bytes(
+        serialization.Encoding.DER,
+        serialization.PublicFormat.SubjectPublicKeyInfo)
+    key_bytes = _get_key_from_asn1(der_bytes)
+
+    # Save private key to a file
+    fn = os.path.join(utl.get_app_data_path("HeyMac"), "heymac.pem")
+    with open(fn, "wb") as f:
+        f.write(prv_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.BestAvailableEncryption(passphrase)))
+    print("Wrote: %s" % fn)
+
+    # Save DER to a file
+    fn = os.path.join(utl.get_app_data_path("HeyMac"), "pub_key.der")
+    with open(fn, "wb") as f:
+        f.write(der_bytes)
+    print("Wrote: %s" % fn)
+
+    # Create a HeyMac identity (credential) file including the public key
     fn = os.path.join(utl.get_app_data_path("HeyMac"), "mac_identity.json")
     with open(fn, 'w') as f:
         json_str = json.dumps({
             "name": name,
             "callsign": callsign,
-            # "pub_key": pub_key.hex()}) # Python 3.5 and later
-            "pub_key": codecs.encode(pub_key, 'hex').decode() # Python 3.4
+            # "key_bytes": key_bytes.hex()}) # Python 3.5 and later
+            "pub_key": codecs.encode(key_bytes, 'hex').decode() # Python 3.4
             })
         f.write(json_str)
     print("Wrote: %s" % fn)
