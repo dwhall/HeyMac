1a052266238447ed7fefba3fe5a9d567cab0e068
!!Dean <dwhall256@gmail.com>
2021-09-17 05:15:28 -0500

    Improve app data utility module
    Follow Python's recipe for platform identification (startswith).
    Add freebsd as an assumed-posix platforms (but it's untested).
    Rename the verb in the data shelve procedure to "open"
    so it better fits Python's context manager (the with statement).
    Add an exception if this module is run on an unsupported platform.

--- heymac/utl/app_data.py
+++ heymac/utl/app_data.py
@@ -9,13 +9,14 @@ import sys
 
 
 def get_app_data_path(app_name):
-    """Returns the OS-specific path to Application Data for the given App
+    """Returns the OS-specific path to Application Data for the given App.
+    Creates the path if it doesn't already exist.
 
     NOTE: Darwin: https://developer.apple.com/reference/foundation/1414224-nssearchpathfordirectoriesindoma?language=objc
     """
     assert type(app_name) == str
 
-    if sys.platform == 'darwin':
+    if sys.platform.startswith('darwin'):
         from AppKit import \
             NSSearchPathForDirectoriesInDomains, \
             NSApplicationSupportDirectory, \
@@ -25,10 +26,17 @@ def get_app_data_path(app_name):
                 NSApplicationSupportDirectory,
                 NSUserDomainMask,
                 True)[0], app_name)
-    elif sys.platform == 'win32':
+
+    elif sys.platform.startswith('win32'):
         app_data_path = os.path.join(os.environ['APPDATA'], app_name)
-    else:
+
+    elif sys.platform.startswith('linux') \
+            or sys.platform.startswith('freebsd'):  # freebsd is untested
         app_data_path = os.path.expanduser(os.path.join("~", "." + app_name))
+
+    else:
+        raise NotImplementedError("The platform, {}, is not supported."
+                                  .format(sys.platform))
 
     if not os.path.exists(app_data_path):
         os.mkdir(app_data_path)
@@ -36,9 +44,9 @@ def get_app_data_path(app_name):
     return app_data_path
 
 
-def get_app_data_shelve(app_name):
-    """Returns a Python shelve object backed by a file in the app data path.
-    The caller MUST close the shelve object.
+def open_app_data_shelve(app_name):
+    """Opens and returns a Python shelve object backed by a file
+    in the app data path.  The caller MUST close the shelve object.
     """
     fn = app_name + ".shelve"
     file_path = os.path.join(get_app_data_path(app_name), fn)
